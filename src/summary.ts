acd11a2dbf6bab45c5f8d92a82c39a0165af3873
Xuemin Guan <xueminguan@Xuemins-MacBook-Pro.local>
2023-03-07 10:48:33 +0000

    XG: use readline to read from commandline

--- src/summary.ts
+++ src/summary.ts
@@ -1,1 +1,15 @@
 console.log("Hello, world.");
+
+const readline = require('readline').createInterface({
+    input: process.stdin
+});
+
+const inputLines: string[] = []
+readline.on('line', (input: string) => {
+    inputLines.push(input)
+})
+readline.on('close', () => {
+    console.log('\nBye!')
+    console.log(inputLines)
+    process.exit(0)
+})
